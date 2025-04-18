097a2016e42268a237b72ca6c7e07b6b6249d401
ge87nat <martin.halfen@tum.de>
2025-04-18 22:28:07 +0200

    CHANGED: - added newton interpolation inplcae

--- src/interpolate/newton.hpp
+++ src/interpolate/newton.hpp
@@ -10,11 +10,45 @@ template <Arithmetic T>
 class Newton final : public Interpolate<T>
 {
 private:
+    std::vector<T> _matrix;
+    std::size_t _size;
+
+    constexpr T divideDiffrence(const T& ci1, const T& ci, const T& xik, const T& xi) const noexcept
+    {
+        return (ci1 - ci) / (xik - xi);
+    }
+    std::vector<T> returnCoeffiecents() const
+    {
+        std::vector<T> coefficents;
+        coefficents.resize(this->_points.size());
+
+        std::vector<T> scratch;
+        scratch.reserve(this->_points.size());
+
+        for (const auto& point : this->_points) {
+            scratch.push_back(point.second);
+        }
+
+        for (size_t i = 1; i < this->_points.size(); i++) {
+            for (size_t j = 0; j < this->_points.size() - i; j++) {
+                scratch[j] = divideDiffrence(scratch[j + 1], scratch[j], this->_points[i + j].first, this->_points[j].first);
+            }
+            coefficents[i] = scratch[0];
+        }
+        return coefficents;
+    }
+
 public:
     Newton() = default;
     Poly<T> getInterpolationPolynom() const override
     {
-        return Poly<T>();
+        if (_size != this->_points.size()) {
+            /* code */
+        }
+
+        Poly<T> p(returnCoeffiecents());
+
+        return p;
     }
 };
 
