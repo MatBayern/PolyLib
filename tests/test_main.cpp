9217b444a38e0a09a3fa4061526b87f763594a37
ge87nat <martin.halfen@tum.de>
2025-04-21 13:05:52 +0200

    CHNGAED: - added more tests

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,25 +1,93 @@
 #include "../src/polyLib.hpp"
 #include <cmath>
+#include <forward_list>
 #include <gtest/gtest.h>
 #include <vector>
 
-TEST(PolynomialTest, simpleTest)
+TEST(PolynomialTestConstructor, empty)
 {
     poly::Poly<double> p;
     ASSERT_EQ(p.getDegree(), 0);
 }
 
-TEST(PolynomialTest, checkCoefficent)
+TEST(PolynomialTestConstructor, VectorCopy)
 {
     std::vector<double> coef = {1, 2, 3, 5};
     poly::Poly<double> p{coef};
     ASSERT_EQ(p.getDegree(), coef.size() - 1);
-    for (size_t i = 0; i < coef.size(); i++)
-    {
+    for (size_t i = 0; i < coef.size(); i++) {
+        ASSERT_DOUBLE_EQ(p[i], coef[i]);
+        // ASSERT_NEAR
+    }
+}
+
+TEST(PolynomialTestConstructor, VectorMove)
+{
+    std::vector<double> coef = {1, 2, 3, 5};
+    std::vector<double> coefCopy{coef};
+
+    poly::Poly<double> p{std::move(coefCopy)};
+    ASSERT_EQ(p.getDegree(), coef.size() - 1);
+    for (size_t i = 0; i < coef.size(); i++) {
         ASSERT_DOUBLE_EQ(p[i], coef[i]);
     }
-    
 }
+
+TEST(PolynomialTestConstructor, Iterator)
+{
+    std::forward_list<double> coef = {1, 2, 3, 5};
+    poly::Poly<double> p{coef.begin(), coef.end()};
+
+    const size_t SIZE = std::distance(coef.begin(), coef.end());
+
+    ASSERT_EQ(p.getDegree(), SIZE - 1);
+    for (size_t i = 0; i < SIZE; i++) {
+        ASSERT_DOUBLE_EQ(p[i], coef.front());
+        coef.pop_front();
+    }
+}
+
+TEST(PolynomialTestConstructor, initializerList)
+{
+    std::vector<double> coef = {1, 2, 3, 5};
+    poly::Poly<double> p = {1, 2, 3, 5};
+
+    ASSERT_EQ(p.getDegree(), 3);
+    for (size_t i = 0; i < 4; i++) {
+        ASSERT_DOUBLE_EQ(p[i], coef[i]);
+    }
+}
+
+TEST(PolynomialTestOperator, Addition)
+{
+    poly::Poly<int> p1 = {1, 2, 3, 5};
+    poly::Poly<int> p2 = {1, 2, 3, 5};
+
+    poly::Poly<int> pResult = {2, 4, 6, 10};
+
+    p1 = p1 + p2;
+
+    ASSERT_EQ(p1.getDegree(), pResult.getDegree());
+    for (size_t i = 0; i <= p1.getDegree(); i++) {
+        ASSERT_EQ(p1[i], pResult[i]);
+    }
+}
+
+TEST(PolynomialTestOperator, Subtraktion)
+{
+    poly::Poly<int> p1 = {1, 2, 3, 5};
+    poly::Poly<int> p2 = {2, 3, 4, 5};
+
+    poly::Poly<int> pResult = {1, 1, 1};
+
+    p1 = p1 - p2;
+
+    ASSERT_EQ(p1.getDegree(), pResult.getDegree());
+    for (size_t i = 0; i <= p1.getDegree(); i++) {
+        ASSERT_EQ(p1[i], pResult[i]);
+    }
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
